5ecd82df2b034183a8e7d34e562168c3272f86ef
John Oliver <johnoliver021@gmail.com>
2018-05-10 11:58:15 -0300

    Add files via upload

--- POO/exercicios/Amigo/Pessoa.java
+++ POO/exercicios/Amigo/Pessoa.java
@@ -23,13 +23,31 @@ public class Pessoa {
 	}
 	@Override
 	public String toString() {
-		if(amigo.getNome().equals(nome)){
-			return "Pessoa [nome=" + nome + ", amigo=" +"ele mesmo" + "]";
-		}else if(amigo == null){
+		if(amigo == null){
 			return "Pessoa [nome=" + nome+", amigo = Não tem Amigo";
+		}else if(amigo.getNome().equals(nome)){
+			return "Pessoa [nome=" + nome + ", amigo=" +"ele mesmo" + "]";
+		
 		}
 		return "Pessoa [nome=" + nome + ", amigo=" + amigo.getNome() + "]";
 	}
+	
+	public String amigoComum(Pessoa amg) {
+		
+		if(this.amigo == null)
+			return "amigos diferentes";
+		else if(amg.getAmigo() == null)
+			return "amigos diferentes";
+		System.out.println("amigo de "+this.nome+" = "+this.amigo.getNome());
+		System.out.println("amigo de "+amg.getNome()+" = "+amg.getAmigo().getNome());
+		if (this.amigo.getNome().equals(amg.getAmigo().getNome()))
+			return "amigos em comum";
+		else
+			return "amigos diferentes";
+		
+		
+	}
+
 	
 	
 	
--- POO/exercicios/Amigo/Teste.java
+++ POO/exercicios/Amigo/Teste.java
@@ -10,12 +10,31 @@ public class Teste {
 	ana.setAmigo(joao);
 	paulo.setAmigo(julia);
 	julia.setAmigo(joao);
-	julia.setAmigo(julia);
 	
 	System.out.println(joao);
 	System.out.println(ana);
 	System.out.println(paulo);
 	System.out.println(julia);
+	
+	paulo.setAmigo(ana);
+	julia.setAmigo(null);
+	ana.setAmigo(ana);
+	
+	
+	System.out.println("-----------------------------");
+	System.out.println(joao);
+	System.out.println(ana);
+	System.out.println(paulo);
+	System.out.println(julia);
+	
+	System.out.println("verificar se paulo tem o mesmo amigo de joão");
+	System.out.println(paulo.amigoComum(joao));
+	
+	System.out.println("--------------------------------------------");
+	System.out.println("verificar se ana tem o mesmo amigo de julia");
+	System.out.println(ana.amigoComum(julia));
+	System.out.println(julia.amigoComum(ana));
+	
 	
 	
 	
