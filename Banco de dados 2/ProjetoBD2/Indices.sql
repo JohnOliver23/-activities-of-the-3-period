eb016096ecdf38087b178a9560b21b1a3630afd4
John Oliver <johnoliver021@gmail.com>
2018-07-16 11:08:17 -0300

    Add files via upload

---
+++ Banco de dados 2/ProjetoBD2/Indices.sql
@@ -0,0 +1,12 @@
+﻿
+/* Indice para o nome dos pacientes */
+ CREATE INDEX ind_nome_pac ON
+paciente(nome);
+
+/* Indice para o uf dos pacientes */
+CREATE INDEX ind_uf_pac ON
+paciente(uf);
+
+/* Indice para o a data de atendimento dos pacientes */
+CREATE INDEX ind_at_pac ON
+atendimento(data_hora);
